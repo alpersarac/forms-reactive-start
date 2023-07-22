1416789b33f15a8ed7e09072f7f28b1ff8836605
alpersarac <alper.sarac42@gmail.com>
2023-07-23 02:00:21 +0300

    Initilasing the form object

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  
   genders = ['male', 'female'];
   signupForm: FormGroup;
+
+  ngOnInit(): void {
+    this.signupForm=new FormGroup({
+      'username': new FormControl(null),
+      'email': new FormGroup(null),
+      'gender': new FormGroup('male')
+    });
+  }
 }
