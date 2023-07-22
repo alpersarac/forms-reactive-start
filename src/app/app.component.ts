eeeec9c6a5affe8cc6729e5b69258dafbed5b63d
alpersarac <alper.sarac42@gmail.com>
2023-07-23 01:35:00 +0300

    Reactive Form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   genders = ['male', 'female'];
+  signupForm: FormGroup;
 }
