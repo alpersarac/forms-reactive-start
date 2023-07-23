05fb6569cd7857019c05cc0ca4ddae30299cf6af
alpersarac <alper.sarac42@gmail.com>
2023-07-23 16:47:01 +0300

    async validation with a value

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -16,7 +17,7 @@ export class AppComponent implements OnInit{
     this.signupForm=new FormGroup({
       'userData': new FormGroup({
         'username': new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
-        'email': new FormControl(null, [Validators.required, Validators.email]),
+        'email': new FormControl(null, [Validators.required, Validators.email],this.forbiddenEmails),
       }),
       'gender': new FormControl('male'),
       'hobbies': new FormArray([])
@@ -37,5 +38,17 @@ export class AppComponent implements OnInit{
       return {'nameIsForbidden':true};
     }
     return null;
+  }
+  forbiddenEmails(control: FormControl): Promise<any> |Observable<any>{
+    const promise = new Promise<any>((resolve,reject)=>{
+      setTimeout(() => {
+        if(control.value==='test@test.com'){
+          resolve({'emailIsForbidden':true});
+        }else{
+          resolve(null)
+        }
+      }, 1500);
+    });
+    return promise;
   }
 }
