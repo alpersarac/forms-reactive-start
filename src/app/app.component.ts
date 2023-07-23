c4ed08909035f8e7ac5d83d701d5c1bfe4d7b588
alpersarac <alper.sarac42@gmail.com>
2023-07-23 16:26:31 +0300

    Specific validation values

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,11 +10,12 @@ export class AppComponent implements OnInit{
 
   genders = ['male', 'female'];
   signupForm: FormGroup;
+  forbiddenUserNames=['Chris','Anna'];
 
   ngOnInit(): void {
     this.signupForm=new FormGroup({
       'userData': new FormGroup({
-        'username': new FormControl(null, Validators.required),
+        'username': new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
         'email': new FormControl(null, [Validators.required, Validators.email]),
       }),
       'gender': new FormControl('male'),
@@ -30,5 +31,11 @@ export class AppComponent implements OnInit{
   }
   getHobbyControls() {
     return (<FormArray>this.signupForm.get('hobbies')).controls;
+  }
+  forbiddenNames(control: FormControl):{[s:string]:boolean}{
+    if(this.forbiddenUserNames.indexOf(control.value)!==-1){
+      return {'nameIsForbidden':true};
+    }
+    return null;
   }
 }
