c2c3d76b24a36ace03e458e0b91ff9fa01ecc873
alpersarac <alper.sarac42@gmail.com>
2023-07-23 15:23:01 +0300

    nested form groups

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,8 +13,10 @@ export class AppComponent implements OnInit{
 
   ngOnInit(): void {
     this.signupForm=new FormGroup({
-      'username': new FormControl(null, Validators.required),
-      'email': new FormControl(null, [Validators.required, Validators.email]),
+      'userData': new FormGroup({
+        'username': new FormControl(null, Validators.required),
+        'email': new FormControl(null, [Validators.required, Validators.email]),
+      }),
       'gender': new FormControl('male')
     });
   }
