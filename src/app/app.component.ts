177d66caae885b2e82ef5ad5b070ab46cea819c2
alpersarac <alper.sarac42@gmail.com>
2023-07-23 15:50:07 +0300

    adding Hobby field with FormArray

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -17,10 +17,18 @@ export class AppComponent implements OnInit{
         'username': new FormControl(null, Validators.required),
         'email': new FormControl(null, [Validators.required, Validators.email]),
       }),
-      'gender': new FormControl('male')
+      'gender': new FormControl('male'),
+      'hobbies': new FormArray([])
     });
   }
   onSubmit(){
     console.log(this.signupForm);
+  }
+  onAddHobby(){
+    const control = new FormControl(null, Validators.required);
+    (<FormArray>this.signupForm.get('hobbies')).push(control)
+  }
+  getHobbyControls() {
+    return (<FormArray>this.signupForm.get('hobbies')).controls;
   }
 }
